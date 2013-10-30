2defc1c8f7c839587fb6803e4f4d18fe730dcb41
Chris <cjsawyercsu@gmail.com>
2013-10-30 16:56:32 -0600

    Moved from Eclipse to jIDEA

---
+++ src/com/mammothGames/wake/game/entities/poolObj_greenOrb.java
@@ -0,0 +1,12 @@
+package com.mammothGames.wake.game.entities;
+
+import android.util.Log;
+
+import com.mammothGames.wake.gameEngine.engine_main;
+import com.mammothGames.wake.gameEngine.engine_reference;
+import com.mammothGames.wake.gameEngine.utility_poolObject;
+
+public class poolObj_greenOrb extends utility_poolObject {
+	public float x = 0, y = 0, speed;
+	public int radius, border_size;
+}
